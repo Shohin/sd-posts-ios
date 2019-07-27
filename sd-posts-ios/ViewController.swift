93e85d233fae4ae4c610133b95d7e4985b7ff211
Shohin <tagayev.shohin@gmail.com>
2019-07-27 10:00:32 +0500

    Initial Commit

---
+++ sd-posts-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sd-posts-ios
+//
+//  Created by Shohin Tagaev on 7/27/19.
+//  Copyright © 2019 super-dispatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
